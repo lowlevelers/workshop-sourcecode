db159a8011aecf5918974880441269098c2665dc
tin-snowflake <cqtin0903@gmail.com>
2024-01-16 17:26:33 +0700

    update template code of merkle tree struct

--- merkle-tree/src/merkle_tree.rs
+++ merkle-tree/src/merkle_tree.rs
@@ -5,4 +5,23 @@ pub struct MerkleTree<T: Hasher> {
     hash: PhantomData<T>,
 }
 
-impl<T> MerkleTree<T> where T: Hasher {}
+impl<T> MerkleTree<T>
+where
+    T: Hasher,
+{
+    pub fn root_hex() -> String {
+        todo!("implement method to get the root hash in hexadecimal")
+    }
+
+    pub fn from_leaves() {
+        todo!("implement method to build the tree from leaves")
+    }
+
+    pub fn verify() {
+        todo!("implement method to verify the proof hash")
+    }
+
+    pub fn print_tree() {
+        todo!("implement method to print the merkle tree levels")
+    }
+}
